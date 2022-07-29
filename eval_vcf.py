b8cf15c0e8f1d5682a699de6e19f0a33d3d6dfed
Tom Delomas <43458918+delomast@users.noreply.github.com>
2022-07-29 16:11:51 -0400

    initial commit

---
+++ eval_vcf.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+# This script is called by the microhap simulation
+# R script to evaluate a VCF file. It returns:
+# He for all SNPs and He for all microhaps.
+# Assumes: imputed, phased data w/o missing genotypes
+#   all loci biallelic
+# 
+# command line arguments in order
+# input vcf file, output prefix, window size
+
+# load libraries
+import sys
+import random
+
+#' @param genoList list of split VCF rows with only genotype columns
+#'   and genotypes are all biallelic
+def calcHe(genoList):
+	# build string representations of haplotypes
+	alleles1 = [""] * len(genoList[0])
+	alleles2 = [""] * len(genoList[0])
+	for g in genoList:
+		alleles1 = [alleles1[i] + g[i][0] for i in range(0, len(alleles1))]
+		alleles2 = [alleles2[i] + g[i][2] for i in range(0, len(alleles2))]
+	# count allele frequencies
+	af = {}
+	for a in alleles1 + alleles2:
+		af[a] = af.get(a, 0) + 1
+	# calc He
+	He = 1
+	for a in af:
+		f = af[a] / (len(alleles1) + len(alleles2))
+		He -= f**2
+	
+	# returning as string b/c is immediately being written to file
+	return str(He)
+
+#' @param genos one split VCF rows with only genotype columns
+def calcSNPHe(genos):
+	# count allele frequencies
+	af = {}
+	for g in genos: # g is each vcf genotype cell
+		af[g[0]] = af.get(g[0], 0) + 1 # g[0] is first character (allele 1)
+		af[g[2]] = af.get(g[2], 0) + 1 # g[2] is third character (allele 2)
+	numAlleles = len(genos) * 2
+	# calc He
+	He = 1
+	for a in af:
+		f = af[a] / numAlleles
+		He -= f**2
+	return(str(He))
+
+def Main():
+	# command line arguments
+	# input file, output prefix, window size ,random seed, number of random SNPs for qtl
+	inFile = sys.argv[1]
+	outPrefix = sys.argv[2]
+	wS = int(sys.argv[3])
+	numRand = int(sys.argv[5])
+	
+	# set random seed
+	random.seed(int(sys.argv[4]))
+	
+	# determine number of SNPs in the file
+	numSNPs = 0
+	chrom = []
+	pos = []
+	with open(inFile, "r") as fileIn:
+		line = fileIn.readline()
+		while line:
+			if line[0] != "#":
+				numSNPs += 1
+			line = fileIn.readline()
+	
+	# choose positions with first SNP being 1
+	qtl = random.sample(range(1,numSNPs + 1), numRand)
+	qtl.sort() # sort list
+	qtl.reverse() # reversing so can use pop
+	
+	#######
+	# Calculate He with sliding window outputs chr, pos, and He to file
+	
+	# run sliding window across input VCF file
+	with open(inFile, "r") as fileIn, open(outPrefix + "HeSNP.txt", "w") as outFile_snp, open(outPrefix + "HeMH.txt", "w") as outFile_mh:
+		
+		# skip header
+		line = fileIn.readline()
+		while line[0] == "#":
+			line = fileIn.readline()
+		
+		# set up beginning of first window
+		lineNum = 1 # "line number" with first SNP being line 1
+		line = line.rstrip().split("\t")
+
+		
+		if len(qtl) > 0:
+			toSkip = qtl.pop()
+		else:
+			toSkip = None
+		while lineNum == toSkip:
+			# chr, pos, lineNum, He, qtl
+			outFile_snp.write("\t".join([line[0], line[1], str(lineNum), calcSNPHe(line[9:]), "TRUE"]) + "\n")
+			line = fileIn.readline()
+			line = line.rstrip().split("\t")
+			lineNum += 1
+			if len(qtl) > 0:
+				toSkip = qtl.pop()
+			else:
+				toSkip = None
+		
+		curChr = line[0] # current chromosome window is on
+		cur = [int(line[1])] # current positions in the window
+		genos = [line[9:]] # genotypes for current window
+		windLines = [lineNum] # "line numbers" for current window
+		
+		
+		
+		# need to incorporate qtl into SNP output and skip qtl for microhap evaluations
+		
+		
+		
+
+		# evaluate first SNP
+		# chr, pos, lineNum, He, qtl
+		outFile_snp.write("\t".join([curChr, line[1], str(lineNum), calcSNPHe(line[9:]), "FALSE"]) + "\n")
+		
+		nextR = fileIn.readline() # next available position
+		lineNum += 1
+		
+		# loop through all possible windows
+		numSnpsPerWindow = {}
+		numWindows = 0
+		while nextR:
+			nextR = nextR.rstrip().split("\t")
+			if lineNum == toSkip:
+				# chr, pos, lineNum, He, qtl
+				outFile_snp.write("\t".join([nextR[0], nextR[1], str(lineNum), calcSNPHe(nextR[9:]), "TRUE"]) + "\n")
+				nextR = fileIn.readline()
+				lineNum += 1
+				if len(qtl) > 0:
+					toSkip = qtl.pop()
+				else:
+					toSkip = None
+				continue
+
+			# evaluate SNP
+			# chr, pos, lineNum, He, qtl
+			outFile_snp.write("\t".join([nextR[0], nextR[1], str(lineNum), calcSNPHe(nextR[9:]), "FALSE"]) + "\n")
+			nextR[1] = int(nextR[1])
+			# determine if the next variant is within the window
+			if nextR[0] == curChr and (nextR[1] - cur[0]) < wS:
+				cur += [nextR[1]] # add to window
+				genos += [nextR[9:]]
+				windLines += [lineNum]
+			else:
+				# evaluate current window
+				numWindows += 1
+				numSnpsPerWindow[len(cur)] = numSnpsPerWindow.get(len(cur), 0) + 1
+
+				# now calculate expHet and write to output
+				outFile_mh.write("\t".join([curChr, ",".join([str(x) for x in cur])] + [calcHe(genos)] + [",".join([str(x) for x in windLines])]) + "\n")
+			
+				# advance to next window
+				if curChr == nextR[0]:
+					# have to include the next snp, otherwise
+					# new window is a subset of the previous window
+					newCur = []
+					newGenos = []
+					newWindLines = []
+					for i in range(0, len(cur)):
+						if (nextR[1] - cur[i]) < wS:
+							newCur += [cur[i]]
+							newGenos += [genos[i]]
+							newWindLines += [windLines[i]]
+					cur = newCur
+					genos = newGenos
+					windLines = newWindLines
+					# adding new SNP
+					cur += [nextR[1]]
+					genos += [nextR[9:]]
+					windLines += [lineNum]
+				else:
+					curChr = nextR[0]
+					cur = [nextR[1]]
+					genos = [nextR[9:]]
+					windLines = [lineNum]
+			nextR = fileIn.readline()
+			lineNum += 1
+			
+		# while loop will end with the last window un-evaluated
+		# evaluate last window
+		numWindows += 1
+		numSnpsPerWindow[len(cur)] = numSnpsPerWindow.get(len(cur), 0) + 1
+		outFile_mh.write("\t".join([curChr, ",".join([str(x) for x in cur])] + [calcHe(genos)] + [",".join([str(x) for x in windLines])]) + "\n")
+		
+		# print some summary information
+		print("number of windows: ", numWindows)
+		print("Distribution of SNPs per window")
+		print("SNPs ", "NumberOfWindows")
+		for k in sorted(list(numSnpsPerWindow)):
+			print(k, numSnpsPerWindow[k])
+
+	
+
+if __name__ == '__main__':
+	Main()
---
+++ utils.R
@@ -0,0 +1,370 @@
+# utility functions used in other script(s)
+
+library(dplyr)
+library(stringr)
+library(optiSel)
+library(AllocateMate)
+
+
+#' when multiple pops from the same sim are in a list
+#' this will pull all genotypes and put them in one matrix
+#' order of loci will be order in alphasimr, not in `loci`
+#' @param popList a list of one or more alphaSimR "Pop"s
+#' @param loci a vector of locus names (column names from `pullSnpGeno`) to select
+all_pullSnpGenos <- function(popList, loci = NULL){
+	if(length(popList) < 1) stop("No pops in popList")
+	g <- pullSnpGeno(popList[[1]])
+	if(is.null(loci)){
+		loci <- colnames(g)
+	} else {
+		g <- g[,colnames(g) %in% loci] #preserve column order
+	}
+	if(length(popList) > 1){
+		for(i in 2:length(pop)){
+			gTemp <- pullSnpGeno(pop[[i]]) #preserve column order
+			g <- rbind(g, gTemp[,colnames(gTemp) %in% loci])
+		}
+	}
+	return(g)
+}
+
+
+#' create G matrix with specified base pop frequencies
+#' first method of VanRaden (2008), also Endelman and Jannink (2012)
+#' @param g genotypes with rows inds, cols loci, coded as 0,1,2, no missing data allowed
+#' @param af base generation frequencies of the non reference allele (the allele whose dosage the number corresponds to)
+#'   in the same order as the columns of `genos`
+createG <- function(g, af){
+	if(length(af) != ncol(g)) stop("wrong af length for number of loci")
+	# P matrix in VanRaden (2008)
+	p <- matrix(2*(af - 0.5), nrow = nrow(g), ncol = ncol(g), byrow = TRUE)
+	return(
+		# subtract 1 to convert to -1,0-1 creating M,
+		# then subtract P from M to create Z
+		# then ZZ' and divide by P to scale G analagous to A
+		tcrossprod((g - 1) - p) / (2 * sum(af *(1 - af)))
+	)
+}
+
+
+#' adds spaces to teh end of strings to make
+#' all strings in the vector the same length (number of characters)
+#' Useful for creating fixed width files, such as for input of genotypes to
+#' blupf90
+#' @param x the character vector
+#' @param pad the character to use to pad the strings
+pad_id <- function(x, pad = " "){
+	if(nchar(pad) != 1) stop("pad must be only one character in length")
+	nc <- nchar(x)
+	m <- max(nc)
+	toAdd <- m - nc
+	uToAdd <- unique(toAdd[toAdd > 0])
+	if(length(uToAdd) < 1) return(as.character(x))
+	for(u in uToAdd){
+		temp <- paste(rep(pad, u), collapse = "")
+		x[toAdd == u] <- paste0(x[toAdd == u], temp)
+	}
+	return(x)
+}
+
+
+#' function to apply full-sib family testing with an AlphaSimR object
+#' pulls ID's and phenotypes for specified proportion (rounded) of individuals from each
+#' full-sib family
+#' 
+#' @param fam an AlphaSimR "Pop-class" object containing full-sib families
+#' @param propTest the proportion of each family to phenotype. One value applied
+#'   to all families. The actual number of indiviudals to phenotype will be rounded.
+sibTestEqual <- function(fam, propTest){
+	fs <- data.frame(mother = fam@mother, father = fam@father) %>% 
+		count(mother, father) %>% mutate(nPheno = round(propTest * n))
+	phenos <- data.frame()
+	for(i in 1:nrow(fs)){
+		# select id
+		toPheno <- sample(fam@id[fam@mother == fs$mother[i] & fam@father == fs$father[i]], size = fs$nPheno[i])
+		temp <- cbind(toPheno, fs$mother[i], fs$father[i], as.data.frame(fam@pheno[match(toPheno, fam@id),]))
+		colnames(temp) <- c("id", "mother", "father", paste0("Trait_", 1:fam@nTraits))
+		phenos <- phenos %>% rbind(temp)
+	}
+	return(phenos)
+}
+
+
+#' Score function for greedy algorithm
+#' from Matukumalli et al. 2009 https://doi.org/10.1371/journal.pone.0005350
+#' @param start start of interval
+#' @param end end of interval
+#' @param maf maf of markers within interval
+#' @param pos position of marker within interval (order
+#'   corresponds to order of `maf`)
+scoreGreedy <- function(start, end, maf, pos){
+	return(
+		maf * (end - start - abs((2 * pos) - end - start))
+	)
+}
+
+
+#' greedy algorithm to choose markers for a genetic panel
+#' from Matukumalli et al. 2009 https://doi.org/10.1371/journal.pone.0005350
+#' except intended to use He in place of maf
+#' Fails if  there are not enough SNPs in each chromosome to meet the requested number
+#' 
+#' @param num data.frame with chromosome name in the "chr" column and
+#'   number of loci to choose in the "num" column (uses column names not positions) adnd
+#'   chr length in the len column
+#' @param locusEval data.frame with columns "chr", "pos", "lineNum", and "He" 
+#'   (created by python vcf evaluation script)
+greedyChooseLoci <- function(num, locusEval){
+
+	panel <- data.frame()
+	for(i in 1:nrow(num)){ # for each chr
+		cands <- locusEval %>% filter(chr == num$chr[i])
+		if(nrow(cands) < num$num[i]){
+			stop("Not enough loci in chromosome ", num$chr[i])
+		}
+		# calculate scores at the start
+		cands <- cands %>% 
+			mutate(wStart = as.numeric(gsub(",.+$", "", pos)),
+					wEnd = as.numeric(gsub("^.+,", "", pos)),
+					score = scoreGreedy(start = 0, end = num$len[i], maf = He, 
+									   pos = wStart),
+					lastStart = 0,
+					lastEnd = num$len[i])
+		numSNPs <- 0
+		while(TRUE){ # for each desired SNP
+			if(nrow(cands) < 1) stop("Ran out of loci in chromosome ", num$chr[i])
+			# choose SNP
+			temp <- which.max(cands$score)
+			chosen <- cands %>% slice(temp)
+			panel <- panel %>% bind_rows(chosen)
+			numSNPs <- numSNPs + 1
+			if(numSNPs == num$num[i]) break
+			
+			# first remove any windows (if microhaps) overlapping with the chosen window
+			# note this also removes the selected locus
+			cands <- cands %>% filter(wStart > chosen$wEnd | wEnd < chosen$wStart)
+
+			# recalculate scores for affected SNPs
+			# only those whose interval used for the last calculation are affected
+			# note that there is only one interval to update b/c the new SNP can only
+			# have been in one interval
+			toUpdate <- cands %>% filter(lastStart < chosen$wEnd & lastEnd > chosen$wStart) %>%
+				select(lastStart, lastEnd) %>% distinct() # this is some bs to help vectorize operations for R
+			tempBool <- cands$lastStart == toUpdate$lastStart & cands$wStart < chosen$wStart
+			cands$score[tempBool] <- scoreGreedy(start = toUpdate$lastStart,
+												end = chosen$wStart,
+												maf = cands$He[tempBool],
+												pos = cands$wStart[tempBool])
+			cands$lastEnd[tempBool] <- chosen$wStart
+			tempBool <- cands$lastStart == toUpdate$lastStart & cands$wStart > chosen$wStart
+			cands$score[tempBool] <- scoreGreedy(start = chosen$wStart,
+												end = toUpdate$lastEnd,
+												maf = cands$He[tempBool],
+												pos = cands$wStart[tempBool])
+			cands$lastStart[tempBool] <- chosen$wStart
+		}
+	}
+	return(panel %>% arrange(chr, wStart))
+}
+
+
+#' read in haplotypes from a vcf file using the line number outputs (first SNP is line number 1)
+#' @param vcfPath path to vcf file
+#' @param lineNumbers line numbers of the loci in the vcf file to keep. 
+#'   First locus in the file is line 1.
+#' @param numLines number of lines of VCF to read at one time
+#' @return a list of a matrix (haplotypes, rows as haplotypes, cols as loci, adjacent 
+#'   rows are individuals) and 
+#'   data.frame of chr and pos (same order as cols of matrix)
+vcf_readLoci <- function(vcfPath, lineNumbers, numLines = 20000){
+	# read in VCF and calculate maf for each locus
+	f <- file(vcfPath, "r") # open vcf
+	on.exit(close(f))
+	# move to end of header
+	genos <- readLines(f, n = numLines)
+	while(length(genos) > 0){
+		endHeader <- which(grepl("^#CHROM", genos))
+		if(length(endHeader) > 0){
+			genos <- genos[endHeader:length(genos)]
+			break
+		}
+		genos <- readLines(f, n = numLines)
+	}
+	# read in individual names
+	indNames <- str_split(genos[1], "\t")[[1]][-(1:9)]
+	genos <- genos[-1] # now it's just locus info (or empty if numLines = 1 or no loci in file)
+	
+	saveGenos <- matrix(nrow = 0, ncol = 2 * length(indNames))
+	saveMap <- data.frame()
+	
+	# now read in chunks
+	lineAdjust <- 0
+	genos <- c(genos, readLines(f, n = numLines - length(line)))
+	while(length(genos) > 0){
+		laNext <- length(genos)
+		lineBool <- (lineAdjust + (1:laNext)) %in% lineNumbers
+		if(sum(lineBool) > 0){
+			# only process lines you need
+			genos <- genos[lineBool]
+			# shorten list of lines to look for
+			lineNumbers <- lineNumbers[!lineNumbers %in% (lineAdjust + (1:laNext)[lineBool])]
+			
+			# splits columns by tabs and genotypes by "|"
+			# so starting with col 10, each col is a haplotype and each pair
+			# of columns is an individual (i.e. col 10 and 11, col 12 and 13, ...)
+			genos <- str_split(genos, "\t|\\|", simplify = TRUE)
+			
+			chrPos <- genos[,1:2] # save position info
+			
+			# now we select only the genotypes and convert to numeric
+			# loci are still rows and haplotypes are columns
+			genos <- matrix(as.numeric(genos[,10:ncol(genos)]), ncol = (ncol(genos) - 9))
+			
+			# check for any with more than 2 alleles 
+			# (note: assumes no NA values)
+			if(any(!(genos %in% c(0,1)))){
+				stop("Possible locus (loci) with more than 2 alleles, missing genotype, or non standard allele coding found. ",
+						 "within loci numbers ", lineAdjust + 1, " - ", lineAdjust + nrow(genos))
+			}
+			
+			# save chr, pos, genos
+			saveGenos <- rbind(saveGenos, genos)
+			saveMap <- rbind(saveMap, data.frame(chr = chrPos[,1],
+																					 pos = as.numeric(chrPos[,2])))
+		}
+		lineAdjust <- lineAdjust + laNext
+		genos <- readLines(f, n = numLines)
+	}
+	
+	return(list(genos = t(saveGenos), map = saveMap))
+}
+
+
+#' @param g a matrix of genotypes (0,1,2) with rownames as indiviudals and colnames as loci
+#' @param founderAlleleFreqs a named vector of allele freqs in the base generation with names as loci
+#' @param localTempDir local temp dir input to script
+#' @param iterationNumber iteration number input to script
+#' @param trainPhenos dataframe with phenotype information: id, mother, father, Trait_1
+#' @param SP_pedigree matrix with pedigree information: rowname is individual, columns 
+#'   include mother, father. Probably should just be `SP$pedigree`
+#' @param curGenIDs a vector of current generation ids (includes selection candidates) that shoudl be included
+#'   in the model (in addition to phenotyped individuals and parents). something like `pop[[gen + 1]]@id`
+#' @return GEBVs with names (from g) in the column "levelNew"
+calcGEBVs_blupf90 <- function(g, founderAlleleFreqs, localTempDir, iterationNumber, trainPhenos,
+							  SP_pedigree, curGenIDs){
+	# write out base generation allele freqs
+	write.table(cbind(1:ncol(g), founderAlleleFreqs[colnames(g)]),
+				paste0(localTempDir, "/", "temp", iterationNumber, "/", "baseFreqs.txt"),
+				sep = " ", col.names = FALSE, row.names = FALSE, quote = FALSE)
+	
+	p <- data.frame(id = rownames(g)) %>% 
+		left_join(trainPhenos %>% select(id, Trait_1) %>% rename(pheno = Trait_1), by = "id") # hard coded for first trait
+	
+	# phenotypes
+	# coding so that all phenotypes are above 100, missing is 0, and including an overall mean
+	p %>% mutate(pheno = pheno + abs(min(min(pheno, na.rm = TRUE), 0)) + 100, mu = 1) %>%
+		filter(!is.na(pheno)) %>% select(id, mu, pheno) %>%
+		write.table(paste0(localTempDir, "/", "temp", iterationNumber, "/", "f90dat.txt"), 
+					sep = " ", col.names = FALSE, row.names = FALSE, quote = FALSE)
+	# genotypes
+	# only include individuals that are either phenotyped, were selected, or are selection candidates
+	allParents <- unique(c(SP_pedigree[,"father"], SP_pedigree[,"mother"]))
+	allParents <- allParents[allParents != 0] # remove founder placeholder
+	# phenotyped, were selected, selection candidates
+	g <- g[rownames(g) %in% c(p$id[!is.na(p$pheno)], allParents, curGenIDs),]
+	rownames(g) <- paste0(pad_id(rownames(g)), " ")
+	write.table(g,
+				paste0(localTempDir, "/", "temp", iterationNumber, "/", "f90snp.txt"), 
+				sep = "", col.names = FALSE, row.names = TRUE, quote = FALSE)
+	rm(g)
+	# estimate gebvs with airemlf90
+	system2(command = "bash", args = c("run_blupf90.sh", paste0(localTempDir, "/", "temp", iterationNumber, "/")))
+	# load in solutions
+	sol <- read.table(paste0(localTempDir, "/", "temp", iterationNumber, "/solutions"), row.names = NULL, skip = 1) %>%
+		filter(V2 == 2) # get only animal effect
+	xref <- read.table(paste0(localTempDir, "/", "temp", iterationNumber, "/f90snp.txt_XrefID"), row.names = NULL)
+	sol$levelNew <- xref$V2[match(sol$V3, xref$V1)] # append original name to solutions
+	
+	return(sol)
+}
+
+
+#' using results of `opticont` from `optiSel` package
+#' calculate number of matings for each individual with proper rounding
+#' distributes rounding error randomly according to weights
+#' but with no individual having more than 1 randomly allocated
+#' to it
+#' @param ocsParent $parent componenet of optisolve result
+#' @param N number of matings to perform
+calcNumMatings <- function(ocsParent, N){
+	# males
+	males <- ocsParent %>% filter(Sex == "male") %>% 
+		mutate(n = round(2 * N * oc))
+	diff <- N - sum(males$n)
+	if(diff != 0){
+		temp <- sample(1:nrow(males), size = abs(diff), prob = males$n, replace = FALSE)
+		if(diff > 0) males$n[temp] <- males$n[temp] + 1
+		if(diff < 0) males$n[temp] <- males$n[temp] - 1
+	}
+	# females
+	females <- ocsParent %>% filter(Sex == "female") %>% 
+		mutate(n = round(2 * N * oc))
+	diff <- N - sum(females$n)
+	if(diff != 0){
+		temp <- sample(1:nrow(females), size = abs(diff), prob = females$n, replace = FALSE)
+		if(diff > 0) females$n[temp] <- females$n[temp] + 1
+		if(diff < 0) females$n[temp] <- females$n[temp] - 1
+	}
+	
+	return(rbind(males, females))
+}
+
+
+#' Calculate the maximum mean kinship corresponding to 
+#' a given effective population size and starting mean
+#' kinship
+#' @param kBar mean kinship at time 0
+#' @param Ne desired effective population size
+#' @param t0 start time
+#' @param t end time
+#' @param L generation length (time)
+ubKin <- function(kBar, Ne, t0 = 0, t = 1, L = 1){
+	return(1 - ((1 - kBar)*((1 - (1 / (2*Ne)))^((t - t0)/L))))
+}
+
+
+#' choose matings with OCS followed by inbreeding 
+#' minimization
+#' @param ocsData a data frame with each row corresponding to
+#'   a selection candidate. Columns are Indiv, Sex (male and female)
+#'   and gebv
+#' @param Gmat genomic relationship matrix (the function internally converts
+#'   this to the coancestry matrix)
+#' @param N the number of matings to be performed
+#' @param Ne the desired minimum effective population size
+runOCS <- function(ocsData, Gmat, N, Ne = 50){
+	# convert to coancestry/kinship matrix
+	# and making sure order/presence of individuals is correct
+	Gmat <- Gmat[ocsData$Indiv,ocsData$Indiv] / 2
+	# data processing
+	ocsCandes <- candes(phen = ocsData, N = N * 2, kin = Gmat)
+	# optimum contributions
+	ocsContrib <- opticont(method = "max.gebv", cand = ocsCandes, 
+												 con = list(ub.kin = ubKin(kBar = ocsCandes$mean$kin, Ne = Ne)), 
+												 trace=FALSE)
+	# calculate number of matings per individual from contribution proportions
+	ocsMatings <- calcNumMatings(ocsParent = ocsContrib$parent, N = N)
+	
+	# assign crosses (limit each pair to one cross)
+	# to minimize inbreeding of each family
+	# This branch and bound algorithm failed frequently with ub.n=1
+	# crosses <- matings(ocsMatings, Kin=Gmat, ub.n = 1)
+	ocsMatings <- ocsMatings %>% mutate(ID=as.character(Indiv), 
+																			 SEX=ifelse(Sex == "male", "M", "F"), 
+																			 EBV=gebv, N_AS_PARENT=n) %>% 
+		select(ID, SEX, EBV, N_AS_PARENT) %>% filter(N_AS_PARENT > 0)
+	crosses <- allocate.mate.H(H = Gmat[ocsMatings$ID, ocsMatings$ID]*2, 
+															parents = ocsMatings, max_F = 1, method = "min_F")
+	
+	return(crosses$optimal_families)
+}
