7224b5e82c9bfdcbbcd4b53954695c31beb29d3f
Tom Delomas <43458918+delomast@users.noreply.github.com>
2023-01-19 15:04:59 -0500

    bug fix for newer version of alphasimr

--- mh_snp_comparison_sim.R
+++ mh_snp_comparison_sim.R
@@ -492,7 +492,7 @@ for(gen in 1:nGenerations){
 									 trainPhenos = trainPhenos, SP_pedigree = SP$pedigree, 
 									 curGenIDs = pop[[gen + 1]]@id)
 			# NOTE: only using _current_ generation to calculate accuracy of gebvs
-			comp <- data.frame(id = pop[[gen + 1]]@id, gv = gv(pop[[gen + 1]])) %>% 
+			comp <- data.frame(id = pop[[gen + 1]]@id, gv = as.vector(gv(pop[[gen + 1]]))) %>% 
 				left_join(data.frame(id = as.character(sol$levelNew), gebv = sol$V4), by = "id") %>%
 				left_join(trainPhenos %>% select(id, Trait_1) %>% rename(pheno = Trait_1), by = "id")
 			
